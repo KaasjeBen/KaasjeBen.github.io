0eafe155194cb562445293dc19df5d572644e84b
Ben <D310581@edu.curio.nl>
2025-05-13 08:56:45 +0200

    opbouw

---
+++ header.php
@@ -0,0 +1,8 @@
+<header>
+    <h1>KaasjeBen.github.io</h1>
+    <nav>
+        <a href="index.php">Home</a>
+        <a href="login.php">log in</a>
+        <a href="flexbox.php">Flex box</a>
+    </nav>
+</header>
---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="nl">
+<?php require_once "head.php"; ?>
+<body>
+    <?php require_once "header.php"; ?>
+    <main>
+        <h2>Welkom bij deze website</h2>
+        <p>Kijk gerust even rond!</p>
+    </main>
+</body>
+</html>
