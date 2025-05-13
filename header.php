d905743b0b610efdb569c7193cb7dc28a51235b5
Ben <D310581@edu.curio.nl>
2025-05-13 09:03:18 +0200

    veranderd naar .html

--- header.php
+++
@@ -1,8 +0,0 @@
-<header>
-    <h1>KaasjeBen.github.io</h1>
-    <nav>
-        <a href="index.php">Home</a>
-        <a href="login.php">log in</a>
-        <a href="flexbox.php">Flex box</a>
-    </nav>
-</header>
--- index.php
+++ index.php
@@ -1,8 +1,20 @@
 <!DOCTYPE html>
 <html lang="nl">
-<?php require_once "head.php"; ?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test-document</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
 <body>
-    <?php require_once "header.php"; ?>
+    <header>
+    <h1>KaasjeBen.github.io</h1>
+    <nav>
+        <a href="index.php">Home</a>
+        <a href="login.php">log in</a>
+        <a href="flexbox.php">Flex box</a>
+    </nav>
+</header>
     <main>
         <h2>Welkom bij deze website</h2>
         <p>Kijk gerust even rond!</p>
